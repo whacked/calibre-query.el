7c59879edbdd5f5e124c2598df953ef2993efb6f
natto <natto@natto-tp.(none)>
2011-08-21 17:48:13 -0400

    cleanup

--- calibre-mode.el
+++ calibre-mode.el
@@ -1,8 +1,4 @@
 
-
-
-
-
 (defvar calibre-root-dir (expand-file-name "~/Calibre Library"))
 (defvar calibre-db (concat calibre-root-dir "/metadata.db"))
 (defun calibre-find ()
@@ -31,29 +27,10 @@
                             "LEFT OUTER JOIN books AS b ON d.book = b.id "
                             whereclause
                             "LIMIT 1"
-                            )
-                    )
-         
-
-         )
+                            )))
     (let* ((query-result (shell-command-to-string
                           (format "%s -separator '\t' '%s' '%s'" sql-sqlite-program calibre-db sql-query)))
            (spl-query-result (split-string query-result "\t"))
            )
       (insert (concat calibre-root-dir "/" (first spl-query-result) "/" (second spl-query-result) "." (downcase (nth 2 spl-query-result))))
-      )
-    ))
-
-
-(let ((sqlite-openandclose-callback (defun sqlite-openandclose-callback (s)
-                                      (when (member 'sqlite-openandclose-callback comint-preoutput-filter-functions)
-                                        (remove-hook 'comint-preoutput-filter-functions 'sqlite-openandclose-callback))
-                                      s))
-      (ignore
-       (add-hook 'comint-preoutput-filter-functions 'sqlite-openandclose-callback))
-      (temp-sqlite-process-buffer (buffer-name (apply 'make-comint "my-sqlite-process"  sql-sqlite-program  nil `(,sql-sqlite-db ))))
-      (temp-output-buffer (buffer-name (get-buffer-create "*my-sqlite-output*")))
-      (my-query "select id from subject;") ;
-      
-      )
-  )
+      )))
