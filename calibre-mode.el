8ef00cd383b799c400807886112835a37b951eeb
natto <natto@natto-tp.(none)>
2011-08-21 22:20:21 -0400

    simple querying and opening found pdf

--- calibre-mode.el
+++ calibre-mode.el
@@ -1,36 +1,89 @@
 
+(defun test ()
+  (interactive)
+  (message (char-to-string (read-char "answer? ")))
+ )
+
 (defvar calibre-root-dir (expand-file-name "~/Calibre Library"))
 (defvar calibre-db (concat calibre-root-dir "/metadata.db"))
-(defun calibre-find ()
+
+;; (shell-command-to-string
+;;  (format "%s -separator '\t' '%s' '%s'" sql-sqlite-program calibre-db ".schema books"))
+
+
+(defun calibre-query (sql-query)
+  (interactive)
+  (shell-command-to-string
+   (format "%s -separator '\t' '%s' '%s'" sql-sqlite-program calibre-db sql-query)))
+
+(defun calibre-build-default-query (whereclause &optional limit)
+  (concat "SELECT "
+          "b.id, b.author_sort, b.path, d.name, d.format, b.pubdate"
+          " FROM data AS d "
+          "LEFT OUTER JOIN books AS b ON d.book = b.id "
+          whereclause
+          (when limit
+            (format "LIMIT %s" limit))
+          ))
+
+(defun calibre-query-by-field (wherefield argstring)
+  (concat "WHERE lower(" wherefield ") LIKE '\\''%%"
+          (format "%s" (downcase argstring))
+          "%%'\\''"
+          ))
+
+(defun calibre-read-query-filter-command ()
   (interactive)
   (let* ((search-string (read-from-minibuffer "search string: "))
-         (spl-arg (split-string search-string ":"))
-         (whereclause
-          (if (< 1 (length spl-arg))
-              (let* ((command (downcase (first spl-arg)))
-                     (argstring (second spl-arg))
-                     (wherefield
-                      (cond ((string= "a" (substring command 0 1))
-                             "b.author_sort")
+         (spl-arg (split-string search-string ":")))
+    (if (< 1 (length spl-arg))
+        (let* ((command (downcase (first spl-arg)))
+               (argstring (second spl-arg))
+               (wherefield
+                (cond ((string= "a" (substring command 0 1))
+                       "b.author_sort")
+                      ((string= "t" (substring command 0 1))
+                       "b.title")
+                      )))
+          (calibre-query-by-field wherefield argstring))
+      (format "WHERE lower(b.author_sort) LIKE '\\''%%%s%%'\\'' OR lower(b.title) LIKE '\\''%%%s%%'\\''"
+              (downcase search-string) (downcase search-string)))))
 
-                            ((string= "t" (substring command 0 1))
-                             "b.title")
-                            )))
-                (concat "WHERE lower(" wherefield ") LIKE '\\''%%"
-                        (format "%s" (downcase argstring))
-                        "%%'\\''"
-                        )
+(defun calibre-list ()
+  (interactive)
+  (message (calibre-query
+            (concat "SELECT b.path FROM books AS b "
+                    (calibre-read-query-filter-command)))))
+
+(defun calibre-find ()
+  (interactive)
+  (let* ((sql-query (calibre-build-default-query (calibre-read-query-filter-command) 1))
+         (query-result (calibre-query sql-query))
+         (spl-query-result (split-string (sql-chomp query-result) "\t"))
+         (calibre-id   (nth 0 spl-query-result))
+         (author-sort  (nth 1 spl-query-result))
+         (book-dir     (nth 2 spl-query-result))
+         (book-name    (nth 3 spl-query-result))
+         (book-format  (downcase (nth 4 spl-query-result)))
+         (book-pubdate (nth 5 spl-query-result))
+         (found-file-path (concat calibre-root-dir "/" book-dir "/" book-name "." book-format))
+         )
+    (if (file-exists-p found-file-path)
+        (let ((opr (char-to-string (read-char
+                                    (concat "found " (second spl-query-result) ". [o]pen open[O]ther [c]itekey [p]ath")))))
+          (cond ((string= "o" opr)
+                 (find-file-other-window found-file-path))
+                ((string= "O" opr)
+                 (find-file-other-frame found-file-path))
+                ((string= "c" opr)
+                 (insert (first (split-string author-sort ",")) (substring book-pubdate 0 4) "id" calibre-id))
+                ((string= "p" opr)
+                 (insert found-file-path "\n"))
                 )
-            (format "WHERE lower(b.author_sort) LIKE '\\''%%%s%%'\\'' OR lower(b.title) LIKE '\\''%%%s%%'\\''"
-                    (downcase search-string) (downcase search-string))))
-         (sql-query (concat "SELECT b.path, d.name, d.format FROM data AS d "
-                            "LEFT OUTER JOIN books AS b ON d.book = b.id "
-                            whereclause
-                            "LIMIT 1"
-                            )))
-    (let* ((query-result (shell-command-to-string
-                          (format "%s -separator '\t' '%s' '%s'" sql-sqlite-program calibre-db sql-query)))
-           (spl-query-result (split-string query-result "\t"))
-           )
-      (insert (concat calibre-root-dir "/" (first spl-query-result) "/" (second spl-query-result) "." (downcase (nth 2 spl-query-result))))
-      )))
+          )
+      (message "didn't find that file"))
+    ))
+
+
+
+
